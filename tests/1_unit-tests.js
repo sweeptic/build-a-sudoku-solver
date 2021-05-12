c12bb474d9b7de63f1d6356dba613c567b7057ce
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-12 12:48:19 +0200

    puzzle string validation unit tests - red stage

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,9 +2,47 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
+const {
+  validPuzzleString,
+  invalidPuzzleString,
+  invalidPuzzleString_moreChar,
+  invalidPuzzleString_lessChar,
+} = require('./cases/puzzle.js');
 let solver;
 
-suite('UnitTests', () => {});
+suite('UnitTests', () => {
+  beforeEach(() => {
+    solver = new Solver();
+  });
+
+  test('Logic handles a valid puzzle string of 81 characters ', () => {
+    const result = solver.validate(validPuzzleString);
+    assert.isObject(result);
+    assert.equal(result.isValid, true);
+    assert.strictEqual(result.error, '');
+  });
+
+  test('Logic handles a puzzle string with invalid characters (not 1-9 or .) ', () => {
+    const result = solver.validate(invalidPuzzleString);
+    assert.isObject(result);
+    assert.equal(result.isValid, false);
+    assert.strictEqual(result.error, 'Invalid characters in puzzle');
+  });
+
+  test('Logic handles a puzzle string that is not 81 characters in length ', () => {
+    const result = solver.validate(invalidPuzzleString_moreChar);
+    assert.isObject(result);
+    assert.equal(result.isValid, false);
+    assert.strictEqual(result.error, 'Expected puzzle to be 81 characters long');
+  });
+
+  test('Logic handles a puzzle string that is not 81 characters in length ', () => {
+    const result = solver.validate(invalidPuzzleString_lessChar);
+    assert.isObject(result);
+    assert.equal(result.isValid, false);
+    assert.strictEqual(result.error, 'Expected puzzle to be 81 characters long');
+  });
+});
 
 /*
 Logic handles a valid puzzle string of 81 characters
