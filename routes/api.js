d076bb39fd1b519989ebec43b18328cea3e38ae9
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-12 13:12:47 +0200

    POST api/solve with invalid string functional test - green stage

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,16 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route('/api/check').post((req, res) => {});
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+  app.route('/api/solve').post((req, res) => {
+    const puzzleString = req.body.puzzle;
+    const validatedPuzzle = solver.validate(puzzleString);
 
-    });
+    if (!validatedPuzzle.isValid) {
+      return res.status(200).json(validatedPuzzle);
+    }
+  });
 };
