6eae6877b26327c7b5478e38725ca31c245d0c29
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-12 13:19:54 +0200

    POST api/solve with incorrect length string functional test - red stage

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
-const { invalidPuzzleString } = require('./cases/puzzle');
+const { invalidPuzzleString, invalidPuzzleString_lessChar } = require('./cases/puzzle');
 
 chai.use(chaiHttp);
 
@@ -20,7 +20,27 @@ suite('Functional Tests', () => {
         assert.isObject(res.body, 'response should be an object');
         assert.property(res.body, 'isValid', 'response should have isValid property');
         assert.property(res.body, 'error', 'response should have error property');
+        assert.isFalse(res.body.isValid);
         assert.equal(res.body.error, 'Invalid characters in puzzle');
+        done();
+      });
+  });
+
+  test('Solve a puzzle with incorrect length: POST request to /api/solve', function (done) {
+    chai
+      .request(server)
+      .post('/api/solve')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({
+        puzzle: invalidPuzzleString_lessChar,
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isObject(res.body, 'response should be an object');
+        assert.property(res.body, 'isValid', 'response should have isValid property');
+        assert.property(res.body, 'error', 'response should have error property');
+        assert.isFalse(res.body.isValid);
+        assert.equal(res.body.error, 'Expected puzzle to be 81 characters long - failed test');
         done();
       });
   });
