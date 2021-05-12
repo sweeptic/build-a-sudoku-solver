4ddde10c8ae88301c58b662d1553f18dc7f66fa2
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-12 12:43:56 +0200

    add commented unit and functional test cases

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,19 @@ const assert = chai.assert;
 const Solver = require('../controllers/sudoku-solver.js');
 let solver;
 
-suite('UnitTests', () => {
+suite('UnitTests', () => {});
 
-});
+/*
+Logic handles a valid puzzle string of 81 characters
+Logic handles a puzzle string with invalid characters (not 1-9 or .)
+Logic handles a puzzle string that is not 81 characters in length
+Logic handles a valid row placement
+Logic handles an invalid row placement
+Logic handles a valid column placement
+Logic handles an invalid column placement
+Logic handles a valid region (3x3 grid) placement
+Logic handles an invalid region (3x3 grid) placement
+Valid puzzle strings pass the solver
+Invalid puzzle strings fail the solver
+Solver returns the the expected solution for an incomplete puzzle
+*/
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,25 @@
-const chai = require("chai");
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
+suite('Functional Tests', () => {});
 
-});
-
+/*
+Solve a puzzle with valid puzzle string: POST request to /api/solve
+Solve a puzzle with missing puzzle string: POST request to /api/solve
+Solve a puzzle with invalid characters: POST request to /api/solve
+Solve a puzzle with incorrect length: POST request to /api/solve
+Solve a puzzle that cannot be solved: POST request to /api/solve
+Check a puzzle placement with all fields: POST request to /api/check
+Check a puzzle placement with single placement conflict: POST request to /api/check
+Check a puzzle placement with multiple placement conflicts: POST request to /api/check
+Check a puzzle placement with all placement conflicts: POST request to /api/check
+Check a puzzle placement with missing required fields: POST request to /api/check
+Check a puzzle placement with invalid characters: POST request to /api/check
+Check a puzzle placement with incorrect length: POST request to /api/check
+Check a puzzle placement with invalid placement coordinate: POST request to /api/check
+Check a puzzle placement with invalid placement value: POST request to /api/check
+*/
