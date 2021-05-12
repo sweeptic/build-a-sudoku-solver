4486b1ba388ed4bca2226cdc9966a2d2f8e41012
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-12 13:12:07 +0200

    POST api/solve with invalid string functional test - red stage

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,16 +2,35 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
+const { invalidPuzzleString } = require('./cases/puzzle');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {});
+suite('Functional Tests', () => {
+  test('Solve a puzzle with invalid characters: POST request to /api/solve', function (done) {
+    chai
+      .request(server)
+      .post('/api/solve')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({
+        puzzle: invalidPuzzleString,
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isObject(res.body, 'response should be an object');
+        assert.property(res.body, 'isValid', 'response should have isValid property');
+        assert.property(res.body, 'error', 'response should have error property');
+        assert.equal(res.body.error, 'Invalid characters in puzzle');
+        done();
+      });
+  });
+});
 
 /*
 Solve a puzzle with valid puzzle string: POST request to /api/solve
 Solve a puzzle with missing puzzle string: POST request to /api/solve
-Solve a puzzle with invalid characters: POST request to /api/solve
-Solve a puzzle with incorrect length: POST request to /api/solve
+*Solve a puzzle with invalid characters: POST request to /api/solve
+*Solve a puzzle with incorrect length: POST request to /api/solve
 Solve a puzzle that cannot be solved: POST request to /api/solve
 Check a puzzle placement with all fields: POST request to /api/check
 Check a puzzle placement with single placement conflict: POST request to /api/check
