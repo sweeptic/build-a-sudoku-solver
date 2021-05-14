07b9c2706a49581b7681ac2ee49f5fb9f869fd24
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-14 12:54:42 +0200

    fix if coordinates get uppercase character

--- controllers/Utils/utils.js
+++ controllers/Utils/utils.js
@@ -1,3 +1,9 @@
 const table = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9 };
 
-export const getRow = row => (table[row] ? table[row] : row);
+export const getRow = row => {
+  let checkRow = row;
+
+  if (/[A-Z]/.test(row)) checkRow = row.toLowerCase();
+
+  return table[checkRow] ? table[checkRow] : checkRow;
+};
