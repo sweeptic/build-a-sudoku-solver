09672c59837e7b26bb620a64207341b7acc7831a
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-14 11:52:11 +0200

    solver returns the expected sol for inc puzzle - unit tests - green stage

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -110,19 +110,27 @@ suite('UnitTests', () => {
     assert.property(result, 'error', 'result should have error property');
     assert.equal(result.error, 'Puzzle cannot be solved');
   });
+
+  test('Solver returns the the expected solution for an incomplete puzzle ', () => {
+    const result = solver.solve(puzzlesAndSolutions[0][0]);
+    const puzzleSolution = puzzlesAndSolutions[0][1];
+    assert.isObject(result);
+    assert.property(result, 'solution', 'result should have solution property');
+    assert.equal(result.solution, puzzleSolution);
+  });
 });
 
 /*
-*Logic handles a valid puzzle string of 81 characters
-*Logic handles a puzzle string with invalid characters (not 1-9 or .)
-*Logic handles a puzzle string that is not 81 characters in length
-*Logic handles a valid row placement
-*Logic handles an invalid row placement
-*Logic handles a valid column placement
-*Logic handles an invalid column placement
-*Logic handles a valid region (3x3 grid) placement
-*Logic handles an invalid region (3x3 grid) placement
-*Valid puzzle strings pass the solver
-*Invalid puzzle strings fail the solver
-Solver returns the the expected solution for an incomplete puzzle
-*/
+ *Logic handles a valid puzzle string of 81 characters
+ *Logic handles a puzzle string with invalid characters (not 1-9 or .)
+ *Logic handles a puzzle string that is not 81 characters in length
+ *Logic handles a valid row placement
+ *Logic handles an invalid row placement
+ *Logic handles a valid column placement
+ *Logic handles an invalid column placement
+ *Logic handles a valid region (3x3 grid) placement
+ *Logic handles an invalid region (3x3 grid) placement
+ *Valid puzzle strings pass the solver
+ *Invalid puzzle strings fail the solver
+ *Solver returns the the expected solution for an incomplete puzzle
+ */
