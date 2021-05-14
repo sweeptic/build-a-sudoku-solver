3e678a12989e28a9104e503d1f67e8cbf0ea1620
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-14 12:03:15 +0200

    POST api/post solve puzzle with valid string - red stage

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings');
 const assert = chai.assert;
 const server = require('../server');
 const {
@@ -341,6 +342,23 @@ suite('Functional Tests', () => {
         done();
       });
   });
+
+  test('Solve a puzzle with valid puzzle string: POST request to /api/solve', function (done) {
+    chai
+      .request(server)
+      .post('/api/solve')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send({
+        puzzle: puzzlesAndSolutions[0][0],
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isObject(res.body, 'response should be an object');
+        assert.property(res.body, 'solution', 'response should have solution property');
+        assert.equal(res.body.solution, puzzlesAndSolutions[0][1]);
+        done();
+      });
+  });
 });
 /*
 Solve a puzzle with valid puzzle string: POST request to /api/solve
@@ -356,5 +374,5 @@ Check a puzzle placement with all fields: POST request to /api/check
 *Check a puzzle placement with invalid characters: POST request to /api/check
 *Check a puzzle placement with incorrect length: POST request to /api/check
 *Check a puzzle placement with invalid placement coordinate: POST request to /api/check
-Check a puzzle placement with invalid placement value: POST request to /api/check
+*Check a puzzle placement with invalid placement value: POST request to /api/check
 */
