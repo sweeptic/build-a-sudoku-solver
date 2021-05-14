59c2c3014e64a3939a43ca1eb14b1aeb52cf8481
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-14 13:15:49 +0200

    add unit test name to unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -17,42 +17,42 @@ suite('UnitTests', () => {
     solver = new Solver();
   });
 
-  test('Logic handles a valid puzzle string of 81 characters ', () => {
+  test('UnitTests Logic handles a valid puzzle string of 81 characters ', () => {
     const result = solver.validate(validPuzzleString);
     assert.isObject(result);
     assert.equal(result.isValid, true);
     assert.strictEqual(result.error, '');
   });
 
-  test('Logic handles a puzzle string with invalid characters (not 1-9 or .) ', () => {
+  test('UnitTests Logic handles a puzzle string with invalid characters (not 1-9 or .) ', () => {
     const result = solver.validate(invalidPuzzleString);
     assert.isObject(result);
     assert.equal(result.isValid, false);
     assert.strictEqual(result.error, 'Invalid characters in puzzle');
   });
 
-  test('Logic handles a puzzle string that is not 81 characters in length ', () => {
+  test('UnitTests Logic handles a puzzle string that is not 81 characters in length ', () => {
     const result = solver.validate(invalidPuzzleString_moreChar);
     assert.isObject(result);
     assert.equal(result.isValid, false);
     assert.strictEqual(result.error, 'Expected puzzle to be 81 characters long');
   });
 
-  test('Logic handles a puzzle string that is not 81 characters in length ', () => {
+  test('UnitTests Logic handles a puzzle string that is not 81 characters in length ', () => {
     const result = solver.validate(invalidPuzzleString_lessChar);
     assert.isObject(result);
     assert.equal(result.isValid, false);
     assert.strictEqual(result.error, 'Expected puzzle to be 81 characters long');
   });
 
-  test('Logic handles a valid row placement ', () => {
+  test('UnitTests Logic handles a valid row placement ', () => {
     const result = solver.checkRowPlacement(validPuzzleString, 'a', 1, 7);
     assert.isObject(result);
     assert.property(result, 'valid', 'result should have valid property');
     assert.isTrue(result.valid);
   });
 
-  test('Logic handles a invalid row placement ', () => {
+  test('UnitTests Logic handles a invalid row placement ', () => {
     const result = solver.checkRowPlacement(validPuzzleString, 'd', 3, 3);
     assert.isObject(result);
     assert.property(result, 'valid', 'result should have valid property');
@@ -62,14 +62,14 @@ suite('UnitTests', () => {
     assert.deepEqual(result.conflict, ['row']);
   });
 
-  test('Logic handles a valid column placement ', () => {
+  test('UnitTests Logic handles a valid column placement ', () => {
     const result = solver.checkColPlacement(validPuzzleString, 'a', 1, 7);
     assert.isObject(result);
     assert.property(result, 'valid', 'result should have valid property');
     assert.isTrue(result.valid);
   });
 
-  test('Logic handles a invalid column placement ', () => {
+  test('UnitTests Logic handles a invalid column placement ', () => {
     const result = solver.checkColPlacement(validPuzzleString, 'a', 1, 6);
     assert.isObject(result);
     assert.property(result, 'valid', 'result should have valid property');
@@ -79,14 +79,14 @@ suite('UnitTests', () => {
     assert.deepEqual(result.conflict, ['column']);
   });
 
-  test('Logic handles a valid region (3x3 grid) placement ', () => {
+  test('UnitTests Logic handles a valid region (3x3 grid) placement ', () => {
     const result = solver.checkRegionPlacement(validPuzzleString, 'a', 1, 7);
     assert.isObject(result);
     assert.property(result, 'valid', 'result should have valid property');
     assert.isTrue(result.valid);
   });
 
-  test('Logic handles a invalid region (3x3 grid) placement ', () => {
+  test('UnitTests Logic handles a invalid region (3x3 grid) placement ', () => {
     const result = solver.checkRegionPlacement(validPuzzleString, 'g', 5, 3);
     assert.isObject(result);
     assert.property(result, 'valid', 'result should have valid property');
@@ -96,7 +96,7 @@ suite('UnitTests', () => {
     assert.deepEqual(result.conflict, ['region']);
   });
 
-  test('Valid puzzle strings pass the solver ', () => {
+  test('UnitTests Valid puzzle strings pass the solver ', () => {
     const result = solver.solve(puzzlesAndSolutions[0][1]);
     const puzzleSolution = puzzlesAndSolutions[0][1];
     assert.isObject(result);
@@ -104,14 +104,14 @@ suite('UnitTests', () => {
     assert.equal(result.solution, puzzleSolution);
   });
 
-  test('Invalid puzzle strings fail the solver ', () => {
+  test('UnitTests Invalid puzzle strings fail the solver ', () => {
     const result = solver.solve(invalidPuzzleStringCannotBSolved);
     assert.isObject(result);
     assert.property(result, 'error', 'result should have error property');
     assert.equal(result.error, 'Puzzle cannot be solved');
   });
 
-  test('Solver returns the the expected solution for an incomplete puzzle ', () => {
+  test('UnitTests Solver returns the the expected solution for an incomplete puzzle ', () => {
     const result = solver.solve(puzzlesAndSolutions[0][0]);
     const puzzleSolution = puzzlesAndSolutions[0][1];
     assert.isObject(result);
